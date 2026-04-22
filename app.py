d8af17b99cccaef9e803a2b0b6ba9d982f34d361
Reenu03 <r20050903@gmail.com>
2026-04-22 22:24:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,33 @@
-from flask import Flask, render_template, request
+import streamlit as st
 import pickle
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-app = Flask(__name__)
-
-# load model and tokenizer
 model = load_model("model.h5")
 tokenizer = pickle.load(open("tokenizer.pkl", "rb"))
 
 MAX_LEN = 40
 
-@app.route("/", methods=["GET", "POST"])
-def home():
-    prediction = None
+st.title("📰 Fake News Detection using LSTM")
 
-    if request.method == "POST":
-        text = request.form["news"]
+input_text = st.text_area("Enter News Content")
 
-        seq = tokenizer.texts_to_sequences([text])
+if st.button("Predict"):
+    if input_text.strip() != "":
+        
+        input_text = input_text.lower()
+
+        seq = tokenizer.texts_to_sequences([input_text])
         padded = pad_sequences(seq, maxlen=MAX_LEN, padding='post', truncating='post')
 
-        pred = model.predict(padded)[0][0]
+        prediction = model.predict(padded)[0][0]
 
-        if pred > 0.5:
-            prediction = "Fake News ❌"
+        if prediction > 0.5:
+            st.error("❌ Fake News")
         else:
-            prediction = "Real News ✅"
+            st.success("✅ Real News")
 
-    return render_template("index.html", prediction=prediction)
+        st.write(f"Confidence: {prediction*100:.2f}%")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    else:
+        st.warning("Please enter some text")
